089800203baa4e745d7ddfcbc76ead1fc17217ae
Asynchro-Epool <qq962643013@gmail.com>
2023-09-03 09:17:04 +0800

    update version to RC3

--- kabuki/__init__.py
+++ kabuki/__init__.py
@@ -5,7 +5,7 @@ from . import analyze
 from . import step_methods as steps
 from . import generate
 
-__version__ = "0.6.5RC1"
+__version__ = "0.6.5RC3"
 
 try:
     from IPython.core.debugger import Tracer
